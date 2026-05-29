bc43839084f7313926e8328d9ec8c1f46aa3ff75
horyce <2272446536@qq.com>
2026-05-29 22:49:46 +0800

    ui: 对齐片段数量显示

--- webview/src/views/SidebarView.vue
+++ webview/src/views/SidebarView.vue
@@ -802,8 +802,7 @@ function handleListScroll() {
             ><polyline points="6 9 12 15 18 9"/></svg>
             <svg class="folder-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>
             <span class="folder-name">{{ folderDisplayName(group.folder) }}</span>
-            <span class="folder-count">{{ group.snippets.length }}</span>
-            <!-- 文件夹操作：默认文件夹仅可见，不可重命名/删除 -->
+            <!-- 文件夹操作：放在数量左侧，使数量始终贴右，与默认文件夹对齐 -->
             <div v-if="group.folder.id !== DEFAULT_FOLDER_ID" class="folder-actions">
               <button class="folder-action-btn" :title="t('folder.rename')" @click.stop="openRenameFolder(group.folder)">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
@@ -812,6 +811,7 @@ function handleListScroll() {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
               </button>
             </div>
+            <span class="folder-count">{{ group.snippets.length }}</span>
           </div>
 
           <!-- 文件夹内片段：折叠时隐藏 -->
